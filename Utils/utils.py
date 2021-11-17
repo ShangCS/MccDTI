bdae2b8c6fe9057c5f45f2666fb900e44efe9da2
ShangCS <94102808+ShangCS@users.noreply.github.com>
2021-11-17 16:20:32 +0900

    Add files via upload

---
+++ Utils/utils.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+class Dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+
+
+def lrelu(x, leak=0.2, name="lrelu"):
+    return tf.maximum(x, leak * x)
+
+
+
+
+
